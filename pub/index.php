4bd5d6fbda996acb9820f129d20663945d5a37b8
LubicBydgoszcz <bekisz43@gmail.com>
2023-03-08 15:09:44 +0100

    twig

--- pub/index.php
+++ pub/index.php
@@ -5,8 +5,22 @@ require_once('./../src/config.php');
 use Steampixel\Route;
 
 Route::add('/', function() {
-    echo "Strona Główna";
+    global $twig;
+    $twig->display("index.html.twig");
 });
+
+Route::add('/upload', function() {
+    global $twig;
+    $twig->display("upload.html.twig");
+});
+
+Route::add('/upload', function() {
+    global $twig;
+    if(isset($_POST['submit'])){
+        Post::upload($_FILES['uploadedFile']['tmp_name']);
+     }
+    $twig->display("index.html.twig");
+}, 'post');
 
 Route::run('/projektmoj/pub');
 ?>
--- src/config.php
+++ src/config.php
@@ -2,4 +2,8 @@
 require_once('./../vendor/autoload.php');
 $db = new mysqli("localhost", "root", "", "bazacms");
 require("Post.class.php");
+
+$loader = new Twig\Loader\FilesystemLoader("./../src/templates");
+
+$twig = new Twig\Environment($loader);
 ?>
