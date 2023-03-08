cb4de432b453070a8a41ee10239533e8d6a78975
LubicBydgoszcz <bekisz43@gmail.com>
2023-03-08 14:14:31 +0100

    steampixel

--- pub/index.php
+++ pub/index.php
@@ -1,24 +1,12 @@
 <?php
-require('./../src/config.php');
 
-?>
+require_once('./../src/config.php');
 
-<form method="post" action="" enctype="multipart/form-data">
-        <label for="uploadedFileInput">
-            Wybierz plik do wgrania na serwer:
-        </label>
-        <input type="file" name="uploadedFile" id="uploadedFileInput">
-        <input type="submit" value="Wyślij plik" name="submit">
-</form>
+use Steampixel\Route;
 
-<?php
- if(isset($_POST['submit'])){
-    Post::upload($_FILES['uploadedFile']['tmp_name']);
- }
-?>
+Route::add('/', function() {
+    echo "Strona Główna";
+});
 
-Ostatni post:
-<pre>
-<?php
-var_dump(Post::getPage());
+Route::run('/projektmoj/pub');
 ?>
--- src/config.php
+++ src/config.php
@@ -1,4 +1,5 @@
 <?php
+require_once('./../vendor/autoload.php');
 $db = new mysqli("localhost", "root", "", "bazacms");
 require("Post.class.php");
 ?>
