fb8c5e1a069ad3ceaffc406ec0bb378adab2a1e0
LubicBydgoszcz <bekisz43@gmail.com>
2023-05-10 14:21:24 +0200

    baza danych

--- bazacms.sql
+++ bazacms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 10 Maj 2023, 14:14
+-- Czas generowania: 10 Maj 2023, 14:20
 -- Wersja serwera: 10.4.24-MariaDB
 -- Wersja PHP: 8.0.19
 
@@ -20,6 +20,97 @@ SET time_zone = "+00:00";
 --
 -- Baza danych: `bazacms`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `post`
+--
+
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL,
+  `timestamp` date NOT NULL,
+  `filename` varchar(196) NOT NULL,
+  `customname` text NOT NULL,
+  `userId` int(11) NOT NULL,
+  `removed` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `password`) VALUES
+(1, 'abc@abc.com', '$argon2i$v=19$m=65536,t=4,p=1$Ri54SDZjQ2l0a1l5MkNRTw$3UzI995Tm65/cWa6e2mgU2LgW5M5uMrjGAnXY7Gi1Jc');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `vote`
+--
+
+CREATE TABLE `vote` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `score` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `vote`
+--
+ALTER TABLE `vote`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `post`
+--
+ALTER TABLE `post`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT dla tabeli `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT dla tabeli `vote`
+--
+ALTER TABLE `vote`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
